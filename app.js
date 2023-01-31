1094d8cd2053e4e6d2fc89040e58ab360718fcb9
Sammy <sammystcherubin@gmail.com>
2023-01-31 19:39:08 +0200

    Changed Auth

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const ejs = require("ejs");
 const port = process.env.PORT || 8080;
 const app = express();
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
 
 const User = new mongoose.model("Users", userSchema);
 
@@ -48,7 +48,7 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
   const newUser = User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password)
   });
 
   newUser.save(function (err) {
@@ -62,7 +62,7 @@ app.post("/register", (req, res) => {
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({ email: username }, (err, foundUser) => {
     if (err) {
